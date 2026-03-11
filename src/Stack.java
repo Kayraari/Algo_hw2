0a27e6e2c2ea55e23ba1c57c12fa8988ed7e11d2
KAYRA ARI <kayra.ari@tedu.edu.tr>
2026-03-11 11:41:04 +0300

    Initial commit

---
+++ src/Stack.java
@@ -0,0 +1,48 @@
+
+    public class Stack {
+        Node top;
+        int size;
+
+        public Stack(Node top) {
+            this.top = top;
+            size = 0;
+        }
+
+        public Node getTop() {
+            return top;
+        }
+
+        public void setTop(Node top) {
+            this.top = top;
+        }
+
+        public void push(Object data) {
+            Node newNode = new Node(data, null);
+            if (top == null) {
+                top = newNode;
+            } else {
+                newNode.setNext(top);
+                top = newNode;
+            }
+            size++;
+        }
+
+        public Object pop() {          // Object instead of int
+            if (top == null) {
+                return null;           // null instead of -1
+            }
+            Object val = top.getData();
+            top = top.getNext();
+            size--;
+            return val;
+        }
+
+        public void printStack() {
+            Node current = top;
+            while (current != null) {
+                System.out.print(current.getData() + " ");
+                current = current.getNext();
+            }
+        }
+
+    }
